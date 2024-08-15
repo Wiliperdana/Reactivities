6dc54bfb8efd5920b2fc32d3263d65a900ad950a
Wiliperdana <yudawillyperdana@gmail.com>
2024-08-15 10:26:56 +0700

    End of Section 18

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -9,6 +9,12 @@ namespace API.Controllers
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
-        }   
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
     }
 }
